db5111ccef150800b48295c9ae4e60efbfb093f2
mn.ikhwan <72797830+mnikhwan@users.noreply.github.com>
2023-01-07 00:01:30 +0700

    Tugas UAS Media Pembelajaran - Materi Tata Surya☀

---
+++ tata-surya/src/js/custom.js
@@ -0,0 +1,93 @@
+var clicks = $('#clicks')[0];
+$('.tombol').mouseenter(function() {
+    clicks.play();
+});
+
+function m1() {
+    var element = document.getElementById("sun");
+    element.classList.remove("hide");
+}
+function m1s() {
+    var element = document.getElementById("sun");
+    element.classList.add("hide");
+}
+
+
+function m2() {
+    var element = document.getElementById("mercury");
+    element.classList.remove("hide");
+}
+function m2s() {
+    var element = document.getElementById("mercury");
+    element.classList.add("hide");
+}
+
+
+function m3() {
+    var element = document.getElementById("venus");
+    element.classList.remove("hide");
+}
+function m3s() {
+    var element = document.getElementById("venus");
+    element.classList.add("hide");
+}
+
+
+function m4() {
+    var element = document.getElementById("earth");
+    element.classList.remove("hide");
+}
+function m4s() {
+    var element = document.getElementById("earth");
+    element.classList.add("hide");
+}
+
+
+function m5() {
+    var element = document.getElementById("mars");
+    element.classList.remove("hide");
+}
+function m5s() {
+    var element = document.getElementById("mars");
+    element.classList.add("hide");
+}
+
+
+function m6() {
+    var element = document.getElementById("jupiter");
+    element.classList.remove("hide");
+}
+function m6s() {
+    var element = document.getElementById("jupiter");
+    element.classList.add("hide");
+}
+
+
+function m7() {
+    var element = document.getElementById("saturn");
+    element.classList.remove("hide");
+}
+function m7s() {
+    var element = document.getElementById("saturn");
+    element.classList.add("hide");
+}
+
+
+function m8() {
+    var element = document.getElementById("uranus");
+    element.classList.remove("hide");
+}
+function m8s() {
+    var element = document.getElementById("uranus");
+    element.classList.add("hide");
+}
+
+
+function m9() {
+    var element = document.getElementById("neptune");
+    element.classList.remove("hide");
+}
+function m9s() {
+    var element = document.getElementById("neptune");
+    element.classList.add("hide");
+}
